c6ad746265a88e09d6e6b1d1af21d7e7c07b50b1
Sandini Pitawala <sandinipitawala1@gmail.com>
2021-06-30 06:12:58 +0530

    Updated wp.component

--- src/component/common/wp.component.js
+++ src/component/common/wp.component.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-
+import moment from 'moment';
 import UserService from "../../service/user.service";
 import WorkshopDetailsService from "../../service/form-workshop-details.service";
-import workshop1 from "../../assets/workshop1.jpg";
-import workshop2 from "../../assets/workshop2.jpg";
-import workshop4 from "../../assets/workshop4.jpg";
-
+import workshop6 from "../../assets/workshop7.jpg";
+import speaker from "../../assets/speaker3.PNG";
 
 export default class WP extends Component {
     constructor(props) {
@@ -43,42 +41,51 @@ export default class WP extends Component {
         })
     }
 
+    dateFix(e){
+        window.moment = moment
+        var date = moment(e);
+        var dateComponent = date.utc().format("MMM Do YY");
+        return dateComponent;
+    }
+
     render() {
         const {
+            e,
             workshopDetailsFormInfos
         } = this.state;
         return (
-            <div className="container">
-                <img src={workshop4} className="card-img-top" alt="1" />
+            <div className="container mt-3">
+                    <img src={workshop6} className="card-img" alt="..."/>
+                            <div className="container-fluid">
+                                <div className="card-body" text-white bg-dark>
+                                {workshopDetailsFormInfos && workshopDetailsFormInfos.map((file, index) => (
+                                <div key={index} >
+                                    {file.approvalStatus === true && (
+                                        
+                                        <center e={file.startDate}>
+                                        <h2 className="card-title">{file.title}</h2>
+                                        <h3 className="card-text">  Starting from {this.dateFix(e)}, Will be held for  {file.noOfDays} days.</h3>
+                                        <h3 className="card-text"><small class="text-muted">{file.place}</small></h3>
+                                        </center>  
+                                    )}
+                                   
+                                    <div className="card">
+                                        <center>
+                                        <h4 >Keynote Speakers</h4> <br></br>
+                                        <div >
+                                        <img className="card-m3" src={speaker} alt="Card image cap"/>
+                                        <div className="card-body">
+                                        <h5 className="card-title">{file.speakers}</h5>
+                                        <h4 className="card-text">{file.speakerInstitutes}</h4>
+                                        </div>
+                                        </div>
+                                        </center>
+                                </div> 
+                                </div>
+                                ))}
+                                </div>
 
-                <br></br><br></br>
-
-                <div className="jumbotron">
-                    <div className="alert bg-transparent">
-                        <h4>Workshop Details needed to be approve</h4>
-                    </div>
-                    <ul className="list-group list-group-flush">
-                        {workshopDetailsFormInfos && workshopDetailsFormInfos.map((file, index) => (
-                            <li className="list-group-item bg-transparent" key={index}>
-                                {file.approvalStatus === true &&(
-                                    <div>
-                                        <h1 className="card-text">{file.title}</h1>
-                                        <h3 className="card-text">Workshop Time : {file.time}</h3>
-                                        <h3 className="card-text">Workshop Place : {file.place}</h3>
-                                        <h5 className="card-text">Workshop Starting date : {file.startDate}</h5>
-                                        <h5 className="card-text">No of days workshop, going to be held : {file.noOfDays}</h5>
-                                        <h5 className="card-text">Speakers of workshop : {file.speakers}</h5>
-                                        <h5 className="card-text">Institutes of speakers : {file.speakerInstitutes}</h5>
-                                        <br></br>
-                                    </div>
-                                )}
-
-                            </li>
-                        ))}
-
-
-                    </ul>
-                </div>
+                                </div>
             </div>
         );
     }
